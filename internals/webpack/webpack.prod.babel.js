604d822d4665ab2c45f8daeb8162013aa8084a55
Bert Spaan <bert.spaan@gmail.com>
2016-07-07 15:15:10 -0400

    updated build dirs

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -21,6 +21,7 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
+    publicPath: '/maps-by-decade/',
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
   },
