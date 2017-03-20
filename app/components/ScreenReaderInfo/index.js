cdbddcc7a29d5f3ec94bfee67457f6b34af2a469
Willa Armstrong <wrmstrng@gmail.com>
2017-03-20 10:42:34 -0400

    shortened copy

--- app/components/ScreenReaderInfo/index.js
+++ app/components/ScreenReaderInfo/index.js
@@ -4,9 +4,7 @@ import { Link } from 'react-router'
 function ScreenReaderInfo (props) {
   return (
     <span className='only-screen-reader'>
-      Information for screen reader users: this page uses interactive
-      maps as a means to geospatially navigate the New York Public Library's map collection.
-      To browse the maps using a traditional list view, see the <Link to='/list'>List page</Link>.
+      For text-based search, visit the <Link to='/list'>List page</Link>. For more information, visit the <Link to='/about'>About page</Link>.
     </span>
   )
 }
